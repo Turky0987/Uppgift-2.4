d1658a7b0e6b8dc5f78dd7d027af96dfa6f44927
taal04003 <taal04003@E-PW0234KH>
2023-09-20 10:16:10 +0200

    Add project files.

---
+++ Uppgift 2.4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System; 
+
+    namespace CarRentalCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur mycket är första personens lön?");
+            int P1L = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hur mycket är andra personens lön?");
+            int P2L = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hur mycket är tredje personens lön?");
+            int P3L = int.Parse(Console.ReadLine());
+
+            int totalcost = (P1L + P2L + P3L) / 3;
+
+            Console.WriteLine($"medellönen = {totalcost}kr");
+        }
+
+
+
+    }
+
+}
